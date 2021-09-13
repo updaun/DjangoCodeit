a87316c9d09b64e7a5d05d91c2b217e1217dabbd
updaun <updauney@daum.net>
2021-09-13 22:03:42 +0900

    add datetime

--- menus/views.py
+++ menus/views.py
@@ -1,7 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, 'menus/index.html')
+    today = str(datetime.now().date())
+    context = {"date":today}
+    return render(request, 'menus/index.html', context=context)
