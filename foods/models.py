d60a2fc7bc18741753e4c34d644955d6b3d7cf1e
updaun <updauney@daum.net>
2021-09-20 16:47:29 +0900

    edit menu models

--- foods/models.py
+++ foods/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Menu(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    name = models.CharField(max_length=80)
+    name_eng = models.CharField(max_length=80)
+    description = models.CharField(max_length=120)
     price = models.IntegerField()
     img_path = models.CharField(max_length=255)
 
